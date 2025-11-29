fed9241bcf3cb89426774bf80be43bbeb2e18dbe
luca-longinotti <luca.longinotti@gmail.com>
2025-11-29 03:38:02 +0100

    table sortable by each column

--- move-dashboard/src/components/SensorsTable.vue
+++ move-dashboard/src/components/SensorsTable.vue
@@ -8,19 +8,42 @@ const props = defineProps({
   }
 });
 
+const sortKey = ref("id");
 const sortAsc = ref(true);
 
+/*
 function sortByName() {
-    sortAsc.value = !sortAsc.value;
+   sortAsc.value = !sortAsc.value;
+}
+*/
+
+function sortIcon(key) {
+    if(key !== sortKey.value) return '';
+    return sortAsc.value ? '▲' : '▼';
+}
+
+function sortBy(key) {
+    if(key === sortKey.value) {
+        sortAsc.value = !sortAsc.value; //same coloumn
+    } else {
+        sortKey.value = key;            //new coloumn
+        sortAsc.value = true;
+    }
 }
 
 const sortedSensors = computed(() => {
     return [...props.sensors].sort((a, b) => {
-        if (sortAsc.value) {
-            return a.name.localeCompare(b.name);
-        } else {
-            return b.name.localeCompare(a.name);
-        }
+
+        let valA = a[sortKey.value];
+        let valB = b[sortKey.value];
+
+        //string normalizaion
+        if(typeof valA === 'string') valA = valA.toLowerCase();
+        if(typeof valB === 'string') valB = valB.toLowerCase();
+
+        if(valA < valB) return sortAsc.value ? -1 : 1;
+        if(valA > valB) return sortAsc.value ? 1 : -1;
+        return 0;
     });
 });
 
@@ -30,19 +53,30 @@ const sortedSensors = computed(() => {
     <table class="min-w-full border-collapse border border-gray-300">
         <thead class="bg-gray-100">
             <tr>
-                <th class="border p-2">ID</th>
-                <th @click="sortByName" class="border p-2">
-                    Name
-                    <span v-if="sortAsc">▲</span>
-                    <span v-else>▼</span>
+                <th @click="sortBy('id')" class="border p-2 cursor-pointer">
+                    ID
+                    <span class="ml-1">{{ sortIcon('id') }}</span>
                 </th>
-                <th class="border p-2">Location</th>
-                <th class="border p-2">Last Value</th>
-                <th class="border p-2">Status</th>
+                <th @click="sortBy('name')" class="border p-2 cursor-pointer">
+                    Name
+                    <span class="ml-1">{{ sortIcon('name') }}</span>
+                </th>
+                <th @click="sortBy('location')" class="border p-2 cursor-pointer">
+                    Location
+                    <span class="ml-1">{{ sortIcon('location') }}</span>
+                </th>
+                <th @click="sortBy('lastValue')" class="border p-2 cursor-pointer">
+                    Last Value
+                    <span class="ml-1">{{ sortIcon('lastValue') }}</span>
+                </th>
+                <th @click="sortBy('status')" class="border p-2 cursor-pointer">
+                    Status
+                    <span class="ml-1">{{ sortIcon('status') }}</span>
+                </th>
             </tr>
         </thead>
     
-        <tbody class="6">
+        <tbody>
             <tr v-for="sensor in sortedSensors" :key="sensor.id" class="hover:bg-gray-50">
                 <td class="border p-2">{{ sensor.id }}</td>
                 <td class="border p-2">{{ sensor.name }}</td>
