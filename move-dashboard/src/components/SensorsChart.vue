7bfb00eb309874f3e547b058d8f72c5831e964e3
luca-longinotti <luca.longinotti@gmail.com>
2025-11-29 15:13:47 +0100

    import plotly and first version of graph

--- move-dashboard/src/components/SensorsChart.vue
+++ move-dashboard/src/components/SensorsChart.vue
@@ -1,6 +1,7 @@
 <script setup>
 
-import {computed} from 'vue';
+import {computed, onMounted, watch, ref} from 'vue';
+import Plotly from 'plotly.js-dist-min';
 
 const props = defineProps({
     sensor: {
@@ -9,15 +10,54 @@ const props = defineProps({
     }
 });
 
+const chartRef = ref(null);
 
 // Prepare data for charting
-const timestamps = computed(() => {
-    return props.sensor.measurements.map(m => m.timestamp);
-});
+const timestamps = computed(() => 
+    props.sensor.measurements.map(m => m.timestamp)
+);
 
-const values = computed(() => {
-    return props.sensor.measurements.map(m => m.disp_mm);
-});
+const values = computed(() => 
+    props.sensor.measurements.map(m => m.disp_mm)
+);
+
+// Render chart when component is mounted or data changes
+function renderChart() {
+    const data = [
+        {
+            x: timestamps.value,
+            y: values.value,
+            type: 'scatter',
+            mode: 'lines+markers',
+            name: props.sensor.id
+        },
+        {
+            x: [timestamps.value[0], timestamps.value.at(-1)],
+            y: [props.sensor.threshold, props.sensor.threshold],
+            type: "scatter",
+            mode: "lines",
+            name: "Threshold",
+            line: { dash: "dash", width: 2 }
+        }
+    ];
+
+    const layout = {
+        title: `Sensor ${props.sensor.name} Measurements`,
+        margin: { t: 30, l: 40, r: 10, b: 30 },
+        xaxis: { title: 'Time' },
+        yaxis: { title: 'Displacement (mm)' },
+        showlegend: true
+    };
+
+    Plotly.newPlot(chartRef.value, data, layout, {responsive: true});
+}
+
+onMounted(renderChart);
+
+// Re-render chart when sensor change
+watch(
+    () => props.sensor, renderChart, { deep: true }
+);
 
 </script>
 
@@ -27,9 +67,9 @@ const values = computed(() => {
 <template>
     <div class="border p-4 rounded-md w-full">
         <h2 class="text-lg font-semibold mb-2">
-            {{ sensor.name }} — Measurements
+            {{ sensor.name }} — Trend last 3 days
         </h2>
 
-    <p class="text-gray-500">Grafico</p>
+    <div ref="chartRef" class="w-full h-80"></div>
     </div>
 </template>
