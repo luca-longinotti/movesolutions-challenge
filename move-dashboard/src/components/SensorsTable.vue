e3023d4859141016a7639616f33c54ec1ce1418b
luca-longinotti <luca.longinotti@gmail.com>
2025-11-29 03:09:18 +0100

    table sortable by name

--- move-dashboard/src/components/SensorsTable.vue
+++ move-dashboard/src/components/SensorsTable.vue
@@ -1,26 +1,49 @@
 <script setup>
-defineProps({
+import { ref,computed } from 'vue';
+
+const props = defineProps({
   sensors: {
     type: Array,
     required: true
   }
 });
+
+const sortAsc = ref(true);
+
+function sortByName() {
+    sortAsc.value = !sortAsc.value;
+}
+
+const sortedSensors = computed(() => {
+    return [...props.sensors].sort((a, b) => {
+        if (sortAsc.value) {
+            return a.name.localeCompare(b.name);
+        } else {
+            return b.name.localeCompare(a.name);
+        }
+    });
+});
+
 </script>
 
 <template>
     <table class="min-w-full border-collapse border border-gray-300">
         <thead class="bg-gray-100">
             <tr>
-                <th class="border p-2 text-left">ID</th>
-                <th class="border p-2 text-left">Name</th>
-                <th class="border p-2 text-left">Location</th>
-                <th class="border p-2 text-left">Last Value</th>
-                <th class="border p-2 text-left">Status</th>
+                <th class="border p-2">ID</th>
+                <th @click="sortByName" class="border p-2">
+                    Name
+                    <span v-if="sortAsc">▲</span>
+                    <span v-else>▼</span>
+                </th>
+                <th class="border p-2">Location</th>
+                <th class="border p-2">Last Value</th>
+                <th class="border p-2">Status</th>
             </tr>
         </thead>
     
         <tbody class="6">
-            <tr v-for="sensor in sensors" :key="sensor.id" class="hover:bg-gray-50">
+            <tr v-for="sensor in sortedSensors" :key="sensor.id" class="hover:bg-gray-50">
                 <td class="border p-2">{{ sensor.id }}</td>
                 <td class="border p-2">{{ sensor.name }}</td>
                 <td class="border p-2">{{ sensor.location }}</td>
