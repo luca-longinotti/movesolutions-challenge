1e3eb65f03dad9a0958e3228c8edb2fc3042505b
luca-longinotti <luca.longinotti@gmail.com>
2025-12-04 17:40:56 +0100

    css fix

--- move-dashboard/src/views/Dashboard.vue
+++ move-dashboard/src/views/Dashboard.vue
@@ -113,7 +113,7 @@ onMounted(loadSensors);
         </SummaryCard>
     </div>
 
-    <div class="flex flex-col lg:flex-row items-start gap-6">
+    <div class="flex flex-col lg:flex-row items-start gap-4">
         <div class="lg:w-1/2 w-full bg-white shadow-md rounded-lg p-4">
             <div class="flex flex-col lg:flex-row gap-4 mb-4">
 
